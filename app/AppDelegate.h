20cc354c9159d532797c490ec671f6cfb7084cb8
谭杰 <tanjie@tanjiedeMac.local>
2016-12-15 11:19:35 +0800

    Initial Commit

---
+++ app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  app
+//
+//  Created by 谭杰 on 2016/12/15.
+//  Copyright © 2016年 谭杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  app
+//
+//  Created by 谭杰 on 2016/12/15.
+//  Copyright © 2016年 谭杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
